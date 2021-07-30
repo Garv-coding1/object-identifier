f574ec47096e02f0701740d05e8ce0afc21a24c8
Garv-coding1 <72982301+Garv-coding1@users.noreply.github.com>
2021-07-30 23:42:16 +0530

    Add files via upload

---
+++ kitchen.js
@@ -0,0 +1,46 @@
+status = "";
+objects = [];
+function preload() {
+    img = loadImage("20210718_125152.jpg");
+}
+
+function setup() {
+    canvas = createCanvas(400, 400);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("s_value").innerHTML = "Detecting Objects";
+}
+
+function draw(){
+    image(img, 0, 0, 400, 400);
+    if (status != "") {
+        for (i=0; i<objects.length; i++) {
+            percent = " "+floor(objects[i].confidence*100) + "%";
+            fill("#FF0000");
+            text(objects[i].label + percent, objects[i].x + 15, objects[i].y+20);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+            document.getElementById("s_value").innerHTML = "Objects Detected";
+        }
+    }
+}
+
+function back() {
+    window.location = 'index.html';
+}
+
+function modelLoaded() {
+    console.log("Model Initialized!");
+    status = true;
+    objectDetector.detect(img, gotResults);
+}
+ function gotResults(error, results) {
+     if (error) {
+         console.error(error);
+     }
+     else {
+         console.log(results);
+         objects = results;
+     }
+ }
---
+++ main.js
@@ -0,0 +1,15 @@
+function desk() {
+    window.location = 'desk.html';
+}
+function book() {
+    window.location = 'book.html';
+}
+function kitchen() {
+    window.location = 'kitchen.html';
+}
+function vegetable() {
+    window.location = 'vegetables.html';
+}
+function bedroom() {
+    window.location = 'bedroom.html';
+}
